2a101fbc8e4e7e97060bbf927335ea675f70a06c
Joey Grinberg <joeygrinberg2006@gmail.com>
2026-06-18 16:39:21 -0400

    Phase 1 (#3): Rename sdf module to initialization.py per issue spec

--- srm_burnback/sdf/__init__.py
+++ srm_burnback/sdf/__init__.py
@@ -1,5 +1,5 @@
 """Signed-distance-field construction and reinitialization."""
 
-from .bruteforce import signed_distance_from_sign
+from .initialization import signed_distance_from_sign
 
 __all__ = ["signed_distance_from_sign"]
